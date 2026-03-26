1842a7d5e4004a709633eabed44584b6d124757f
novarol <novarol3d@gmail.com>
2026-03-26 15:23:50 +0100

    test: add script to run tests, add test cases for TextNode

---
+++ src/test_textnode.py
@@ -0,0 +1,39 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+  def test_eq(self):
+    node = TextNode("This is a text node", TextType.BOLD)
+    node2 = TextNode("This is a text node", TextType.BOLD)
+    self.assertEqual(node, node2)
+
+  def test_eq_url(self):
+    node = TextNode("This is a link text node", TextType.LINK, "https://example.com/")
+    node2 = TextNode("This is a link text node", TextType.LINK, "https://example.com/")
+    self.assertEqual(node, node2)
+
+  def test_eq_url_none(self):
+    node = TextNode("This is a text node", TextType.ITALIC)
+    node2 = TextNode("This is a text node", TextType.ITALIC, None)
+    self.assertEqual(node, node2)
+
+  def test_not_eq_text(self):
+    node = TextNode("This is a text node", TextType.PLAIN)
+    node2 = TextNode("This is another text node", TextType.PLAIN)
+    self.assertNotEqual(node, node2)
+
+  def test_not_eq_url(self):
+    node = TextNode("This is a link text node", TextType.IMAGE, "https://test.com/")
+    node2 = TextNode("This is a link text node", TextType.IMAGE, "https://example.com/")
+    self.assertNotEqual(node, node2)
+
+  def test_not_eq_type(self):
+    node = TextNode("This is a text node", TextType.CODE)
+    node2 = TextNode("This is a text node", TextType.BOLD)
+    self.assertNotEqual(node, node2)
+
+
+if __name__ == "__main__":
+  unittest.main()
