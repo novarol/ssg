5041e1f62bdefce2683b95b15609bb77e8567aac
novarol <novarol3d@gmail.com>
2026-03-31 16:07:55 +0200

    feat: add to_html_node method on TextNode

--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,8 @@
 from enum import Enum
 from typing import Self
 
+from leafnode import LeafNode
+
 class TextType(Enum):
   PLAIN = "plain"
   BOLD = "bold"
@@ -14,6 +16,21 @@ class TextNode:
     self.text = text
     self.text_type = text_type
     self.url = url
+
+  def to_html_node(self):
+    match self.text_type:
+      case TextType.PLAIN:
+        return LeafNode(None, self.text)
+      case TextType.BOLD:
+        return LeafNode("b", self.text)
+      case TextType.ITALIC:
+        return LeafNode("i", self.text)
+      case TextType.CODE:
+        return LeafNode("code", self.text)
+      case TextType.LINK:
+        return LeafNode("a", self.text, { "href": self.url })
+      case TextType.IMAGE:
+        return LeafNode("img", "", { "src": self.url, "alt": self.text })
 
   def __eq__(self, other: Self):
     return self.text == other.text and self.text_type == other.text_type and self.url == other.url
